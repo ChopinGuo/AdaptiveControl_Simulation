b9a7a893a20dd8dca3c774c9b8ca0c30c1c3dcdc
ChopinGuo <guoshaopan@pku.edu.cn>
2017-12-31 21:57:47 +0800

    Add files
    可调增益MIT-MRAC归一化算法
    
    可调增益Lyapunov-MRAC

---
+++ chap3_03_MIT_MRAC_Standard.m
@@ -0,0 +1,90 @@
+% 可调增益MIT-MRAC归一化算法
+
+clear
+close all;
+
+% 数值积分步长、仿真步数
+h = 0.1;
+L = 100/h;
+
+% 对象参数
+num = [1];
+den = [1 1 1];
+n = length(den) - 1;
+
+% 传递函数型转换为状态空间型
+kp = 1;
+[Ap, Bp, Cp, Dp] = tf2ss(kp*num, den);
+
+% 参考模型参数
+km = 1;
+[Am, Bm, Cm, Dm] = tf2ss(km*num, den);
+
+% 自适应增益
+gamma = 0.1;
+alpha = 0.01;
+beta = 2;
+
+% 初值
+yr0 = 0;
+u0 = 0;
+e0 = 0;
+ym0 = 0;
+
+% 状态向量初值
+xp0 = zeros(n,1);
+xm0 = zeros(n,1);
+
+% 可调增益初值
+kc0 = 0;
+r = 1.2;
+
+% 输入信号
+yr = r*[ones(1,L/4) -ones(1,L/4) ones(1,L/4) -ones(1,L/4)];
+
+for k=1:L
+    time(k) = k*h;
+    xp(:,k) = xp0 + h*(Ap*xp0 + Bp*u0);
+    yp(k) = Cp*xp(:,k) + Dp*u0;   % 计算yp
+    
+    xm(:,k) = xm0 + h*(Am*xm0 + Bm*yr0);
+    ym(k) = Cm*xm(:,k) + Dm*yr0;  % 计算ym
+    
+    e(k) = ym(k) - yp(k);         % e = ym - yp
+    
+    
+    
+    
+%     kc = kc0 + h*gamma*e0*ym0;    % MIT自适应律
+%     u(k) = kc*yr(k);              % 控制量
+
+
+
+    DD = e0*ym0/km/(alpha+(ym0/km)^2);
+    if DD<-beta
+        DD = -beta
+    end
+    
+    if DD>beta
+        DD = beta
+    end
+    
+    kc = kc0 + h*gamma*DD; % MIT自适应律
+    u(k)= kc*yr(k);        % 控制量
+    
+    % 更新数据
+    yr0 = yr(k);
+    u0 = u(k);
+    e0 = e(k);
+    ym0 = ym(k);
+    xp0 = xp(:,k);
+    xm0 = xm(:,k);
+    kc0 = kc;
+end
+
+plot(time,ym,'r',time,yp,':');
+xlabel('t');
+ylabel('y_m(t)、y_p(t)');
+
+% axis([0 L*h -10 10]);
+legend('y_m(t)', 'y_p(t)');
